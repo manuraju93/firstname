06e89ee5dabb59690ad3ad89444b45c08430a92a
manu <1293manuraju@gmail.com>
2022-11-28 02:38:28 +0000

    modifying script for sub also'

--- add.sh
+++ add.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
-
 echo "ENTER THE NUMBERS"
 read NUM1 NUM2 
-
-
 SUM=`expr $NUM1 + $NUM2 `
 MUL=`expr $NUM1 \* $NUM2`
-
 echo "SUM OF $NUM1,$NUM2 is $SUM"
 echo "MUL OF $NUM1,$NUM2 is $MUL"
+
+SUB=`expr $NUM1 - $NUM2`
+echo "SUB OF $NUM1,$NUM2 is $SUB"
