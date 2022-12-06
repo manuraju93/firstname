681950925705d5fcfffc573d07c4c44e7530509c
manu <1293manuraju@gmail.com>
2022-12-06 02:39:03 +0000

    odd_even.sh

--- odd_even.sh
+++ odd_even.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 declare -a even_list=()
 declare -a odd_list=()
-
 for i in {1..100}
 do
 	if [ `expr $i % 2` == 0 ];
@@ -12,8 +10,7 @@ do
 		odd_list+=($i)
 	fi
 done
-
 echo "list of even numbers:${even_list[@]}"
 echo "list of odd numbers:${odd_list[@]}"
 echo "#############################"
-echo "${even_list[24]}"
+echo "${even_list[2]}"
